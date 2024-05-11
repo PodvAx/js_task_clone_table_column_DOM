d7fb35f42e7233546c7527339d9910be3ce78848
Oleksandr Podvoiskyi <podvoyskiy01@gmail.com>
2024-05-11 23:52:19 +0300

    make build

---
+++ index.efbaa528.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("table");["thead","tbody","tfoot"].forEach(function(t){e.querySelectorAll("".concat(t," tr")).forEach(function(e){var t=e.children[1].cloneNode(!0);e.insertBefore(t,e.lastElementChild)})});
+//# sourceMappingURL=index.efbaa528.js.map
